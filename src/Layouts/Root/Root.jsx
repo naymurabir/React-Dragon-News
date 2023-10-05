481b9f127da1de9944eecae4fedc3a81bcf546f7
naymurabir <naymurrahman.abir@gmail.com>
2023-10-06 00:34:47 +0600

    modify

--- src/Layouts/Root/Root.jsx
+++ src/Layouts/Root/Root.jsx
@@ -1,18 +1,10 @@
 import { Outlet } from "react-router-dom";
-import { Helmet, HelmetProvider } from 'react-helmet-async';
-
 
 const Root = () => {
     return (
-        <HelmetProvider>
-            <Helmet>
-                <title>Home</title>
-            </Helmet>
-
-            <div className="max-w-screen-xl mx-auto px:2 md:px-8 lg:px-16 font-poppins py-5">
-                <Outlet></Outlet>
-            </div>
-        </HelmetProvider>
+        <div className="max-w-screen-xl mx-auto px:2 md:px-8 lg:px-16 font-poppins py-5">
+            <Outlet></Outlet>
+        </div>
     );
 };
 
